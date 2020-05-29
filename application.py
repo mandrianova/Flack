4818b56bffac9db3270023b2d41d238d4c22b488
mandrianova <margo.andr@gmail.com>
2020-05-29 19:48:33 +0300

    first

---
+++ application.py
@@ -0,0 +1,97 @@
+import os
+from datetime import datetime
+
+from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, emit, send
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+users = []
+channels = []
+
+messages = {}
+names = {'users': users, 'channels': channels}
+
+
+def get_chat_name(chat_type, chat_n, user_name):
+    if chat_type == 'to_user':
+        names_chat = [user_name, chat_n]
+        names_chat.sort()
+        chat_name = '{}_{}'.format(names_chat[0], names_chat[1])
+    else:
+        chat_name = chat_n
+    return chat_name
+
+
+@app.route("/")
+def index():
+    print(users, channels)
+    return render_template("index.html", users=sorted(users), channels=sorted(channels))
+
+
+@app.route("/chat/<string:chat_type>/<string:chat_name>")
+def chat(chat_type, chat_name):
+    if (chat_type == "to_user" and chat_name in users) or (chat_type == "channel" and chat_name in channels):
+        return render_template("index.html", users=sorted(users), channels=sorted(channels), chat_name=chat_name)
+    else:
+        return render_template("index.html", users=sorted(users), channels=sorted(channels), error='chat not found')
+
+
+@app.route('/has/<string:type_name>', methods=['POST'])
+def has_name(type_name):
+    if request.form.get('name') in names[type_name]:
+        return jsonify({"success": True, 'name': True})
+    else:
+        return jsonify({"success": True, 'name': False})
+
+
+@socketio.on("join")
+def on_join(data):
+    names[data['type_name']].append(data['name'])
+    if len(names[data['type_name']]) > 100:
+        del names[data['type_name']][0]
+    emit("joined", {'users': sorted(users), 'channels': sorted(channels)}, broadcast=True)
+
+
+@socketio.on("send")
+def send_message(data):
+    if data['user_name'] and data['chat_type'] and data['chat']:
+        chat_name = get_chat_name(data['chat_type'], data['chat'], data['user_name'])
+        if not messages.get(chat_name):
+            messages[chat_name] = []
+        date = str(datetime.now())[0: -7]
+        messages[chat_name].append({'date': date, 'user': data['user_name'], 'message': data['message']})
+        if len(messages[chat_name]) > 100:
+            del messages[chat_name][0]
+        emit("chat", {'chat': chat_name}, broadcast=True)
+    else:
+        emit("error", {'error': "Please, log in and select chat."})
+
+
+@socketio.on("delete_message")
+def delete_message(data):
+    if data['user_name'] and data['chat_type'] and data['chat']:
+        chat_name = get_chat_name(data['chat_type'], data['chat'], data['user_name'])
+        message_id = int(data['id'])
+        if messages.get(chat_name) and messages[chat_name][message_id]:
+            del messages[chat_name][message_id]
+            emit('chat', {"chat": chat_name}, broadcast=True)
+        else:
+            emit("error", {'error': "Message not found."})
+    else:
+        emit("error", {'error': "Please, log in and select chat."})
+
+
+@app.route('/api/chat/<string:chat_name>', methods=['GET'])
+def get_messages(chat_name):
+    if not request.args.get('chat_type') or not request.args.get('user_name'):
+        return jsonify({"success": False, 'error': "Invalid data"})
+    if request.args.get('chat_type') == 'to_user':
+        names_chat = [chat_name, request.args.get('user_name')]
+        names_chat.sort()
+        chat_name = '{}_{}'.format(names_chat[0], names_chat[1])
+    if not messages.get(chat_name):
+        messages[chat_name] = []
+    return jsonify({"success": True, 'messages': messages[chat_name]})
---
+++ static/js/index.js
@@ -0,0 +1,292 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Global variables
+    let user_name = localStorage.getItem('username');
+    let g_chat_type;
+    let g_chat;
+
+    function get_height() {
+        document.querySelector('#chat_window').style.height = window.innerHeight - 270 + "px";
+        document.querySelector('#chats_list').style.height = window.innerHeight - 80 + "px";
+    }
+    get_height();
+    window.addEventListener("resize", get_height);
+    // Function for locking button if input is empty
+    function lock_button(selector_input, selector_button) {
+        if (document.querySelector(selector_input).value.length > 0)
+        {
+            document.querySelector(selector_button).disabled = false;
+        }
+        else
+        {
+            document.querySelector(selector_button).disabled = true;
+        }
+    }
+
+    // Function for change current username
+    function update_username() {
+        user_name = localStorage.getItem('username');
+        document.querySelector('#name').removeAttribute('hidden');
+        document.querySelector('#login').setAttribute('hidden', true);
+        document.querySelector('#p_name').innerHTML = user_name;
+    }
+
+    // Function for getting message for chat
+    function open_chat(chat, chat_type) {
+        document.querySelector('#chat_window').removeAttribute('hidden');
+        document.querySelector('#chat_name').innerHTML = chat;
+        document.querySelector('#messages').innerHTML = "";
+        const request = new XMLHttpRequest();
+            request.open('GET', `/api/chat/${chat}?chat_type=${chat_type}&user_name=${user_name}`);
+            request.onload = () => {
+                const resp = JSON.parse(request.responseText);
+                if (!resp.success) {
+                    document.querySelector('.alert').removeAttribute('hidden');
+                    document.querySelector('#error').innerHTML = resp.error;
+                }
+                else {
+                    document.querySelector('#messages').innerHTML = "";
+                    if (resp.messages.length === 0)
+                    {
+                        document.querySelector('#messages').innerHTML = "No messages";
+                    }
+                    else
+                    {
+                        for (let i = 0; i < resp.messages.length; i++)
+                        {
+                            const message = document.createElement('div');
+                            const user = document.createElement('h6');
+                            const date = document.createElement('small');
+                            const text = document.createElement('div');
+                            user.innerHTML = resp.messages[i]['user'];
+                            date.innerHTML = moment(resp.messages[i]['date']).fromNow();
+                            text.innerHTML = resp.messages[i]['message'];
+                            if (user_name !== resp.messages[i]['user'])
+                            {
+                                message.append(user);
+                                message.className = 'message_body';
+                            }
+                            else {
+                                message.className = 'message_body from_me';
+                                const del = document.createElement('span');
+                                del.className = "delete_message material-icons message-icon";
+                                del.innerHTML = 'delete';
+                                del.setAttribute('data-id', `${i}`);
+                                message.append(del);
+                            }
+                            message.append(text);
+                            message.append(date);
+                            if (i === resp.messages.length - 1)
+                            {
+                                message.setAttribute('id', 'new_message');
+                            }
+                            document.querySelector('#messages').append(message);
+                        }
+                        document.querySelectorAll('.delete_message').forEach(span => {
+                            span.onclick = () => {
+                                let message_id = span.getAttribute('data-id');
+                                socket.emit('delete_message', {'chat': g_chat, 'chat_type': g_chat_type, 'id': message_id, 'user_name': user_name});
+                                open_chat(g_chat, g_chat_type);
+                            };
+                        });
+                    }
+                }
+                document.title = chat;
+                history.pushState({'title': chat, 'text': resp}, chat, `/chat/${chat_type}/${chat}`);
+            };
+            g_chat_type = chat_type;
+            g_chat = chat;
+            activate_chat();
+            request.send();
+    }
+
+    // Check current url
+    let path = window.location.pathname
+
+    // If chat open get chat content
+    if (path.includes("chat")) {
+        let chat_type = path.split("/")[2];
+        let chat_name = path.split("/")[3];
+        g_chat_type = chat_type;
+        g_chat = chat_name;
+        open_chat(chat_name, chat_type);
+    }
+    document.querySelector('.navbar-brand').onclick = () => {
+        document.title = 'Flack';
+        history.pushState({'title': 'Flack'}, 'Flack', `/`);
+        return false;
+    };
+
+
+    // By default, submit button is disabled
+    document.querySelector('#join').disabled = true;
+    document.querySelector('#add_channel').disabled = true;
+    document.querySelector('#send').disabled = true;
+    document.querySelector('#username').onkeyup = function () { lock_button('#username', '#join'); };
+    document.querySelector('#channel').onkeyup = function () { lock_button('#channel', '#add_channel'); };
+    document.querySelector('#message').onkeyup = function () { lock_button('#message', '#send'); };
+
+    document.querySelector('body').onclick = () => {
+        document.querySelector('.alert').setAttribute('hidden', true);
+    };
+
+    // Check username in LocalStorage
+    if (localStorage.getItem('username')) {
+        update_username();
+        document.querySelector('#logout').onclick = function () {
+            localStorage.clear();
+            user_name = null;
+            document.querySelector('#name').setAttribute('hidden', true);
+            document.querySelector('#login').removeAttribute('hidden');
+        };
+    }
+    else {
+        document.querySelector('#name').setAttribute('hidden', true);
+        document.querySelector('#login').removeAttribute('hidden');
+    }
+
+    // Connect to websocket
+    const socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    // When connected, configure buttons
+    socket.on('connect', () => {
+
+        // function for add users and channels
+        function add_name (selector_input, selector_button, type_name){
+            const name = document.querySelector(selector_input).value;
+            const request = new XMLHttpRequest();
+            request.open('POST', `/has/${type_name}`);
+            request.onload = () => {
+                const resp = JSON.parse(request.responseText);
+                if (resp.name) {
+                    document.querySelector('.alert').removeAttribute('hidden');
+                    document.querySelector('#error').innerHTML = "This name is busy!";
+                }
+                else {
+                    socket.emit('join', {'type_name': type_name, 'name': name});
+                    document.querySelector('.alert').removeAttribute('hidden');
+                    document.querySelector('#error').innerHTML = "Success!";
+                    if (type_name === 'users'){
+                        localStorage.setItem('username', name);
+                        update_username();
+                    }
+                }
+            };
+            const names = new FormData();
+            names.append('name', name);
+            request.send(names);
+            document.querySelector(selector_input).value = '';
+            document.querySelector(selector_button).disabled = true;
+        }
+
+        // User join
+        document.querySelector('#join').onclick = function () {add_name('#username', '#join', 'users'); };
+
+        // Add channel
+        document.querySelector('#add_channel').onclick = function () { add_name('#channel', '#add_channel', 'channels'); };
+
+        // Send message
+        document.querySelector('#send').onclick = () => {
+            const text = document.querySelector('#message').value;
+            const chat_type = g_chat_type;
+            const chat = g_chat;
+            socket.emit('send', {'chat_type': chat_type, 'user_name': user_name, 'chat': chat, 'message': text});
+            open_chat(chat, chat_type);
+            document.querySelector('#message').value = '';
+            document.querySelector('#send').disabled = true;
+        };
+    });
+
+
+    // Add new channels and new users
+    socket.on('joined', data => {
+        document.querySelector('.users').innerHTML= '';
+        for (let i = 0; i < data.users.length; i++)
+        {
+            const p = document.createElement('p');
+            p.className = "user_link";
+            p.innerHTML = data.users[i];
+            document.querySelector('.users').append(p);
+
+        }
+        document.querySelector('.channels').innerHTML= '';
+        for (let i = 0; i < data.channels.length; i++)
+        {
+            const p = document.createElement('p');
+            p.className = "channel_link";
+            p.innerHTML = data.channels[i];
+            document.querySelector('.channels').append(p);
+        }
+        get_links();
+        activate_chat();
+    });
+    socket.on('chat', data => {
+       if (data.chat === g_chat && g_chat_type === 'channel') {
+           open_chat(g_chat, g_chat_type);
+       }
+       if (g_chat_type === 'to_user' && (data.chat === `${user_name}_${g_chat}` || data.chat === `${g_chat}_${user_name}`)) {
+           open_chat(g_chat, g_chat_type);
+       }
+    });
+    socket.on('error', data => {
+       document.querySelector('#error').innerHTML = data.error;
+    });
+    function get_links() {
+        document.querySelectorAll('.channel_link').forEach(link => {
+           link.onclick = () => {
+               const chat = link.innerHTML;
+               link.style.animationPlayState = 'running';
+               open_chat(chat, 'channel');
+               return false;
+           };
+        });
+        document.querySelectorAll('.user_link').forEach(link => {
+           link.onclick = () => {
+               const chat = link.innerHTML;
+               open_chat(chat, 'to_user');
+               return false;
+           };
+        });
+    }
+    function activate_chat() {
+        if (g_chat_type === 'channel')
+        {
+            document.querySelectorAll('.channel_link').forEach(p => {
+                if (p.innerHTML === g_chat)
+                    p.setAttribute('id', 'active_chat');
+                else
+                    p.removeAttribute('id');
+            });
+            document.querySelectorAll('.user_link').forEach(p_u => {
+                p_u.removeAttribute('id');
+            });
+        }
+        else
+        {
+            document.querySelectorAll('.user_link').forEach(p => {
+                if (p.innerHTML === g_chat)
+                    p.setAttribute('id', 'active_chat');
+                else
+                    p.removeAttribute('id');
+            });
+            document.querySelectorAll('.channel_link').forEach(p_c => {
+                p_c.removeAttribute('id');
+            });
+        }
+    }
+    function key_enter(input_name, button_name) {
+        let input = document.querySelector(input_name);
+        input.addEventListener("keyup", function (event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                document.querySelector(button_name).click();
+            }
+        });
+    }
+    key_enter('#message','#send');
+    key_enter('#channel','#add_channel');
+    key_enter('#username','#join');
+    activate_chat();
+    get_links();
+
+});
+
